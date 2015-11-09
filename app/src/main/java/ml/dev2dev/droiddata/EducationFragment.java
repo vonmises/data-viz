eedf017de0eda9247870abb0f0816686d942ce56
vonmises <vonmises@users.noreply.github.com>
2015-11-09 04:02:09 +0300

    removed unused imports and variables

--- app/src/main/java/ml/dev2dev/droiddata/EducationFragment.java
+++ app/src/main/java/ml/dev2dev/droiddata/EducationFragment.java
@@ -4,20 +4,13 @@ package ml.dev2dev.droiddata;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class EducationFragment extends Fragment {
     int color;
-    SimpleRecyclerAdapter adapter;
 
     public EducationFragment() {
     }
