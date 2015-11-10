93595046aa67d0fc392ccb8389725761004e28f2
vonmises <vonmises@users.noreply.github.com>
2015-11-10 15:26:50 +0300

    moved the background colour settings from java to xml

--- app/src/main/java/ml/dev2dev/droiddata/DisasterFragment.java
+++ app/src/main/java/ml/dev2dev/droiddata/DisasterFragment.java
@@ -1,33 +1,19 @@
 package ml.dev2dev.droiddata;
 
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 public class DisasterFragment extends Fragment {
-    int color;
 
     public DisasterFragment() {
-    }
-
-    @SuppressLint("ValidFragment")
-    public DisasterFragment(int color) {
-        this.color = color;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.disaster_fragment, container, false);
-
-        final LinearLayout frameLayout = (LinearLayout) view.findViewById(R.id.disaster_frag);
-        frameLayout.setBackgroundColor(color);
-
         return view;
     }
 }
--- app/src/main/java/ml/dev2dev/droiddata/EducationFragment.java
+++ app/src/main/java/ml/dev2dev/droiddata/EducationFragment.java
@@ -1,32 +1,19 @@
 package ml.dev2dev.droiddata;
 
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 public class EducationFragment extends Fragment {
-    int color;
 
     public EducationFragment() {
-    }
-
-    @SuppressLint("ValidFragment")
-    public EducationFragment(int color) {
-        this.color = color;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.education_fragment, container, false);
-
-        final LinearLayout frameLayout = (LinearLayout) view.findViewById(R.id.education_frag);
-        frameLayout.setBackgroundColor(color);
-
         return view;
     }
 }
